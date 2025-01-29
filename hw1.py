8fdfdf80d0c0c4c1507aba060fe177096b407140
Lucita82 <lucita82@msn.com>
2025-01-29 12:01:00 -0600

    Add files via upload
    Intro to Python class homework1

---
+++ hw1.py
@@ -0,0 +1,46 @@
+# hw1.py
+# Name:  Luz Mayen
+# Collaborators:  None
+# References: hw1.pdf, CSC401 Week1 video links--https://d2l.depaul.edu/d2l/le/content/999283/Home?itemIdentifier=D2L.LE.Content.ContentObject.ModuleCO-11253069 , Perkovic, Ljubomir. Introduction to Computing Using Python: An Application Development Focus, 2nd Edition (p. 45). Wiley. Kindle Edition.
+
+
+## 1 ##
+>>> s1*3+s2*5
+## 2 ##
+>>> (s1*2+s2*3)*5
+## 3 ##
+>>> side=x**y
+## 4 ##
+>>> length*width>=100
+## 5 ##
+>>> weeksComplete=days//no_weeks
+## 6 ##
+>>> n==n%2 or n==n%5
+## 7 ##
+>>> 'w' in s[0]
+## 8 ##
+>>> 's' in s[-1]
+## 9 ##
+>>> 'a' in s[(middle)]
+## 10 ##
+>>> len(word1)>len(word2)
+## 11 ##
+>>> max(numbers)-min(numbers)==numbers[-1]
+## 12 ##
+>>> 'pear' in fruit
+## 13 ##
+>>> fruit[0:3]
+## 14 ##
+>>> fruit[-1]
+## 15 ##
+>>> fruit[0:3]+[fruit[-1]]
+## 16 ##
+>>> flips.count('H')
+## 17 ##
+>>> flips.count('H')/100
+## 18 ##
+>>> flips.index('T')
+## 19 ##
+>>> s[::-1]
+## 20 ##
+>>> (x-a)**2+(y-b)**2<r**2
